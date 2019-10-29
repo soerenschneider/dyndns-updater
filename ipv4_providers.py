fcd528f3a097a2d4bb25bd1516ac88361076cf60
soeren <gitlab-please-fix-issue-43521@gmx.de>
2019-10-29 19:46:09 +0100

    Linting

--- ipv4_providers.py
+++ ipv4_providers.py
@@ -2,25 +2,30 @@ import requests
 
 
 def ipify_org():
+    """ IP Provider for ipify.org """
     resp = requests.get('https://api.ipify.org')
     return resp.text, resp.status_code
 
 
 def ident_me():
+    """ IP Provider for v4.ident.me """
     resp = requests.get('https://v4.ident.me/')
     return resp.text, resp.status_code
 
 
 def seeip_org():
+    """ IP Provider for ipv4.seeip.org """
     resp = requests.get('https://ip4.seeip.org/')
     return resp.text, resp.status_code
 
 
 def whatismyipaddress_com():
+    """ IP Provider for ipv4bot.whatismyipaddress.com """
     resp = requests.get('http://ipv4bot.whatismyipaddress.com/')
     return resp.text, resp.status_code
 
 
 def ip_sb():
+    """ IP Provider for api-ipv4.ip.sb """
     resp = requests.get('https://api-ipv4.ip.sb/ip')
     return resp.text, resp.status_code
