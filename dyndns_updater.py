7dc75555ce1e14f1e2761cfa5e3e91b87fc180f2
soeren <2378192-soerenschneider@users.noreply.gitlab.com>
2019-12-31 17:51:08 +0100

    Refactored main logic into two distinct subcomponents

--- dyndns_updater.py
+++ dyndns_updater.py
@@ -1,7 +1,5 @@
 import random
-import hashlib
 import logging
-import json
 import ipaddress
 import time
 
@@ -15,23 +13,17 @@ prom_ipresolver_failed = Counter('dnsclient_ipresolver_failed_total', 'Amount of
 prom_last_check = Gauge('dnsclient_last_check_ts_seconds', 'Timestamp of the latest check for a new IP')
 prom_update_detected = Counter('dnsclient_updates_detected_total', 'Amount of IP updates detected')
 prom_update_detected_ts = Gauge('dnsclient_last_detected_update_ts_seconds', 'Timestamp of update')
-prom_update_request_status_code = Counter('dnsclient_update_requests_total', 'Status code of request', ['status_code'])
 prom_backend_errors = Counter('dnsclient_backend_errors_total', 'Errors with backend interaction', ['operation', 'backend_name'])
 
-class DyndnsUpdater:
-    def __init__(self, dns_record, host, shared_secret, ip_providers, interval=None, persistence=None):
-        self._set_dns_record(dns_record)
 
-        if not host:
-            raise ValueError("host not specified")
-        self.host = host
+class UpdateDetector:
+    def __init__(self, update_notifier, ip_providers, interval=None, persistence=None):
+        if not update_notifier:
+            raise ValueError("No update_notifier configured")
+        self.update_notifier = update_notifier
 
-        if not shared_secret:
-            raise ValueError("secret not specified")
-        self.shared_secret = shared_secret
-        
         if not ip_providers:
-            raise ValueError("No providers for determing IP address found. ")
+            raise ValueError("No providers for determing IP address found")
         self.ip_providers = ip_providers
         
         if not interval:
@@ -43,61 +35,12 @@ class DyndnsUpdater:
         self.persistence_backend = persistence
 
         self._quit = False
-
-    def _set_dns_record(self, dns_record):
-        """
-        Set the DNS record. The model requires the record to end with a dot,
-        so make sure here it is set.
-        """
-        if not dns_record:
-            raise ValueError("dns_record not specified")
-
-        if not dns_record.endswith("."):
-            dns_record += "."
-        self.dns_record = dns_record
-
-
+    
     @staticmethod
     def shuffle_providers(providers):
         """ Makes sure we're using the providers more or less evenly. """
         if providers:
             random.shuffle(providers)
-
-    @staticmethod
-    def hash_request(host, external_ip, shared_secret):
-        """ 'Sign' our request with the shared secret. """
-        if not host or not external_ip or not shared_secret:
-            raise ValueError("Uninitialized value provided")
-
-        return hashlib.sha256(f"{host}{external_ip}{shared_secret}".encode('utf-8')).hexdigest()
-
-    def _build_request(self, external_ip):
-        """ Create request object to send to the endpoint. """
-
-        if not external_ip:
-            raise ValueError("Can not build request object, external_ip is missing")
-        
-        payload = dict()
-        payload["validation_hash"] = DyndnsUpdater.hash_request(self.dns_record, external_ip, self.shared_secret)
-        payload["dns_record"] = self.dns_record
-        payload["public_ip"] = external_ip
-        return payload
-
-    @backoff.on_exception(backoff.expo, requests.exceptions.RequestException, max_tries=5)
-    def _send_update(self, payload):
-        """ Notify the server about an updated IP address. """
-        if not payload:
-            raise ValueError("payload must not be empty")
-
-        logging.info("Sending update to remote server")
-
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        response = requests.post(self.host, data=json.dumps(payload), headers=headers)
-        
-        prom_update_request_status_code.labels(response.status_code).inc()
-        logging.debug("Response from remote: %s", response.status_code)
-        if response.status_code >= 400:
-            raise ValueError()
 
     @staticmethod
     def is_valid_ipv4(ip: str) -> bool:
@@ -129,7 +72,7 @@ class DyndnsUpdater:
         
         for provider in self.ip_providers:
             try:
-                external_ip, status_code = DyndnsUpdater.request_wrapper(provider[1])
+                external_ip, status_code = UpdateDetector.request_wrapper(provider[1])
                 prom_ipresolver_status.labels(provider[0], status_code).inc()
                 if not external_ip:
                     return None
@@ -137,9 +80,8 @@ class DyndnsUpdater:
                 external_ip = external_ip.strip()
 
                 # before proceeding make sure this provider didn't provide garbage
-                if DyndnsUpdater.is_valid_ipv4(external_ip) is True and status_code < 400:
+                if UpdateDetector.is_valid_ipv4(external_ip) is True and status_code < 400:
                     return external_ip
-
             except Exception:
                 logging.debug("Failed to fetch information from provider '%s'", provider[0])
 
@@ -164,8 +106,7 @@ class DyndnsUpdater:
         fetched_ip = self.get_external_ip()
 
         if self.has_update_occured(last_ip, fetched_ip) is True:
-            payload = self._build_request(fetched_ip)
-            self._send_update(payload)
+            self.update_notifier.notify_update(fetched_ip)
             self._write_to_persistence_backend(fetched_ip)
             
         return fetched_ip
@@ -194,7 +135,7 @@ class DyndnsUpdater:
 
         last_ip = self._read_from_persistence_backend()
         logging.info("Read %s from persistence backend", last_ip)
-        
+
         while not self._quit:
             try:
                 last_ip = self.perform_check(last_ip)
--- tests/test_ip.py
+++ tests/test_ip.py
@@ -1,28 +1,28 @@
 from unittest import TestCase
 
-from dyndns_updater import DyndnsUpdater
+from dyndns_updater import UpdateDetector
 
 class TestIp(TestCase):
     def test_is_ipv4_valid(self):
-        response = DyndnsUpdater.is_valid_ipv4("192.168.0.1")
+        response = UpdateDetector.is_valid_ipv4("192.168.0.1")
         self.assertTrue(response)
     
     def test_is_ipv4_valid_2(self):
-        response = DyndnsUpdater.is_valid_ipv4("8.8.8.8")
+        response = UpdateDetector.is_valid_ipv4("8.8.8.8")
         self.assertTrue(response)
 
     def test_is_ipv4_invalid_ipv6(self):
-        response = DyndnsUpdater.is_valid_ipv4("2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+        response = UpdateDetector.is_valid_ipv4("2001:0db8:85a3:0000:0000:8a2e:0370:7334")
         self.assertFalse(response)
 
     def test_is_ipv4_invalid_empty(self):
-        response = DyndnsUpdater.is_valid_ipv4("")
+        response = UpdateDetector.is_valid_ipv4("")
         self.assertFalse(response)
 
     def test_is_ipv4_invalid_none(self):
-        response = DyndnsUpdater.is_valid_ipv4(None)
+        response = UpdateDetector.is_valid_ipv4(None)
         self.assertFalse(response)
 
     def test_is_ipv4_invalid_almost_ipv4(self):
-        response = DyndnsUpdater.is_valid_ipv4("192.168.0")
+        response = UpdateDetector.is_valid_ipv4("192.168.0")
         self.assertFalse(response)
